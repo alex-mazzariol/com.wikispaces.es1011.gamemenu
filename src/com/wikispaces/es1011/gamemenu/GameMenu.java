f63e45f5b961c40dea38ae398bef8ebfef0d1818
alexmaz87 <alex.maz@gmx.com>
2011-03-29 16:32:17 +0200

    Inizio del progetto

---
+++ src/com/wikispaces/es1011/gamemenu/GameMenu.java
@@ -0,0 +1,13 @@
+package com.wikispaces.es1011.gamemenu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameMenu extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
