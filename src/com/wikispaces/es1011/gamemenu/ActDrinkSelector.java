a3ffc38c058875afad1829fb4046825f65b044b1
azzuamarill <azzuamarill@gmail.com>
2011-03-30 11:10:10 +0200

    aggiunta prima bozza del codice drink selector

--- src/com/wikispaces/es1011/gamemenu/ActDrinkSelector.java
+++ src/com/wikispaces/es1011/gamemenu/ActDrinkSelector.java
@@ -1,16 +1,27 @@
 package com.wikispaces.es1011.gamemenu;
 
 import android.app.Activity;
-import android.os.*;
-import android.widget.*;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
+import android.view.*;
 
 public class ActDrinkSelector extends Activity {
-	public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	 @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.actdrinkselector);
 
-        TextView textview = new TextView(this);
-        textview.setText("This is the Artists tab");
-        setContentView(textview);
-    }
+	        GridView gridview = (GridView) findViewById(R.id.gridview);
+	        gridview.setAdapter(new ImageAdapter(this));
+
+	        gridview.setOnItemClickListener(new OnItemClickListener() {
+	            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+	                Toast.makeText(ActDrinkSelector.this, "" + position, Toast.LENGTH_SHORT).show();
+	            }
+	        });	
+	    }
 
 }
