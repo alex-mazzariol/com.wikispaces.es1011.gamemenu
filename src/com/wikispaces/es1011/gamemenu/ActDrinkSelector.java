2f9a8ca529e13d635b4e6c8e50304a9010658f87
alexmaz87 <alex.maz@gmx.com>
2011-03-30 09:54:42 +0200

    Inizio dell'impostazione a tab

---
+++ src/com/wikispaces/es1011/gamemenu/ActDrinkSelector.java
@@ -0,0 +1,16 @@
+package com.wikispaces.es1011.gamemenu;
+
+import android.app.Activity;
+import android.os.*;
+import android.widget.*;
+
+public class ActDrinkSelector extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView textview = new TextView(this);
+        textview.setText("This is the Artists tab");
+        setContentView(textview);
+    }
+
+}
--- src/com/wikispaces/es1011/gamemenu/GameMenu.java
+++ src/com/wikispaces/es1011/gamemenu/GameMenu.java
@@ -1,14 +1,45 @@
 package com.wikispaces.es1011.gamemenu;
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.app.*;
+import android.widget.*;
+import android.content.*;
+import android.content.res.*;
+import android.os.*;
 
-public class GameMenu extends Activity {
+public class GameMenu extends TabActivity {
     /** Called when the activity is first created. */
     @Override
-    //Commento
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Resources res = getResources(); // Resource object to get Drawables
+        TabHost tabHost = getTabHost();  // The activity TabHost
+        TabHost.TabSpec spec;  // Reusable TabSpec for each tab
+        Intent intent;  // Reusable Intent for each tab
+
+        // Create an Intent to launch an Activity for the tab (to be reused)
+        intent = new Intent().setClass(this, ActDrinkSelector.class);
+
+        // Initialize a TabSpec for each tab and add it to the TabHost
+        spec = tabHost.newTabSpec("drinkSelector").setIndicator("Drinks",
+                          res.getDrawable(R.drawable.ic_tab_actdrinkselector ))
+                      .setContent(intent);
+        tabHost.addTab(spec);
+
+        // Do the same for the other tabs
+        intent = new Intent().setClass(this, ActDrinkSuggest.class);
+        spec = tabHost.newTabSpec("drinkSuggest").setIndicator("Suggest",
+                          res.getDrawable(R.drawable.ic_tab_actdrinksuggest))
+                      .setContent(intent);
+        tabHost.addTab(spec);
+
+        intent = new Intent().setClass(this, ActGameEntertainer.class);
+        spec = tabHost.newTabSpec("game").setIndicator("Game",
+                          res.getDrawable(R.drawable.ic_tab_actgameentertainer))
+                      .setContent(intent);
+        tabHost.addTab(spec);
+
+        tabHost.setCurrentTab(2);
     }
 }
