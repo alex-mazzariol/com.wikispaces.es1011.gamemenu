971260dad4267054de904381efaaa59eacbcec6b
alexmaz87 <alex.maz@gmx.com>
2011-04-04 16:31:00 +0200

    Test di layout waitercall

--- src/com/wikispaces/es1011/gamemenu/ActWaiterCall.java
+++ src/com/wikispaces/es1011/gamemenu/ActWaiterCall.java
@@ -1,81 +1,31 @@
 package com.wikispaces.es1011.gamemenu;
 
-import java.io.IOException;
-
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.PixelFormat;
-import android.hardware.Camera;
 import android.os.*;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
-import android.view.View.OnClickListener;
 
-public class ActWaiterCall extends Activity implements SurfaceHolder.Callback, OnClickListener {
-	static final int FOTO_MODE = 0;
-	Camera mCamera;
-	boolean mPreviewRunning = false;
+
+public class ActWaiterCall extends Activity {
+	
 	private SurfaceView mSurfaceView;
 	private SurfaceHolder mSurfaceHolder;
-
+	private CameraShooter csCamera;
+	
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
-
+		csCamera = new CameraShooter(this);
+		
 		getWindow().setFormat(PixelFormat.TRANSLUCENT);
+		
 		setContentView(R.layout.actwaitercall);
+		
 		mSurfaceView = (SurfaceView) findViewById(R.id.actwaitercall_camera);
-		mSurfaceView.setOnClickListener(this);
+		mSurfaceView.setOnClickListener(csCamera);
 		mSurfaceHolder = mSurfaceView.getHolder();
-		mSurfaceHolder.addCallback(this);
+		mSurfaceHolder.addCallback(csCamera);
 		mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-	}
-
-	Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
-		public void onPictureTaken(byte[] imageData, Camera c) {
-
-			if (imageData != null) {
-
-				Intent mIntent = new Intent();
-
-				//FileUtilities.StoreByteImage(mContext, imageData,
-				//		 50, "ImageName");
-				
-				mCamera.startPreview();
-				
-				setResult(FOTO_MODE,mIntent);
-				//finish();
-			}
-		}
-	};
-
-	public void surfaceCreated(SurfaceHolder holder) {
-		mCamera = Camera.open();
-	}
-
-	public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-		if (mPreviewRunning) {
-			mCamera.stopPreview();
-		}
-
-		Camera.Parameters p = mCamera.getParameters();
-		p.setPreviewSize(w, h);
-		mCamera.setParameters(p);
-		try {
-			mCamera.setPreviewDisplay(holder);
-		} catch (IOException e) {}
-		mCamera.startPreview();
-		mPreviewRunning = true;
-	}
-
-	public void surfaceDestroyed(SurfaceHolder holder) {
-		mCamera.stopPreview();
-		mPreviewRunning = false;
-		mCamera.release();
-	}
-
-	public void onClick(View arg0) {
-		mCamera.takePicture(null, mPictureCallback, mPictureCallback);
 	}
 
 }
