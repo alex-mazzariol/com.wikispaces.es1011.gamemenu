34df672f50919ce24afa441bc70b3df18e2ba1d8
azzuamarill <azzuamarill@gmail.com>
2011-04-05 14:55:21 +0200

    modificato il layout del drink selector aggiungendo il testo sotto alle immagini

--- src/com/wikispaces/es1011/gamemenu/ImageAdapter.java
+++ src/com/wikispaces/es1011/gamemenu/ImageAdapter.java
@@ -6,6 +6,8 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class ImageAdapter extends BaseAdapter {
@@ -30,18 +32,30 @@ public class ImageAdapter extends BaseAdapter {
     // create a new ImageView for each item referenced by the Adapter
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-        if (convertView == null) {  // if it's not recycled, initialize some attributes
-            imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+    	ImageView imageView;
+    	TextView tvLabel;
+    	LinearLayout llIcon;
+        
+    	if (convertView == null) {  // if it's not recycled, initialize some attributes
+        	llIcon = new LinearLayout(mContext);
+            llIcon.setOrientation(1);
+        	imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(70, 70));
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
             imageView.setPadding(8, 8, 8, 8);
-        } else {
-            imageView = (ImageView) convertView;
+            imageView.setImageResource(mThumbIds[position]);
+            tvLabel = new TextView(mContext);
+            tvLabel.setLayoutParams(new GridView.LayoutParams(70, 20));
+            tvLabel.setText("testo");
+            llIcon.addView(imageView, 0);
+            llIcon.addView(tvLabel, 1);
+    	} 
+        
+        else {
+        	llIcon = (LinearLayout) convertView;
         }
 
-        imageView.setImageResource(mThumbIds[position]);
-        return imageView;
+    	return llIcon;
     }
 
     // references to our images
